c8ba0dda837723bf3899550222ef0d7b940d78be
GSinghh <Gbassans@yahoo.com>
2023-08-28 15:35:33 -0700

    completed exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    var newString = '';
+
+    for(var i = word.length - 1; i >= 0; i--)
+    {
+        newString += word[i];
+    }
+
+    return newString
 
 };
-
 // Do not edit below this line
 module.exports = reverseString;

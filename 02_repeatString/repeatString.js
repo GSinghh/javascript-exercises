ce234e05a3b0e3d20860a7a5d3cb46b4ff83bc41
GSinghh <Gbassans@yahoo.com>
2023-08-28 13:24:11 -0700

    Completed exercise 1 & 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(word, numRepeat) {
+    var tempWord = '';
 
+    if(numRepeat < 0)
+    {
+        return 'ERROR';
+    }
+    for(var i = 0; i < numRepeat; i++)
+    {
+        tempWord += word;
+    }
+
+    return tempWord;
 };
 
 // Do not edit below this line

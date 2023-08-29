aaad81418c8084a884a54c00636e619577c61c5a
GSinghh <Gbassans@yahoo.com>
2023-08-29 10:39:47 -0700

    Completed exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    var bookNames = [];
+    for(var i = 0; i < books.length; i++)
+    {
+        bookNames.push(books[i].title);
+    }
+    return bookNames;
 };
 
 // Do not edit below this line

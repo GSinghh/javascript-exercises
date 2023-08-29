9dd0e3a9e24ceed75b97efff5c46b857e2ecfe66
GSinghh <Gbassans@yahoo.com>
2023-08-29 11:35:05 -0700

    Completed exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,29 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    var oldestAge = Number.MIN_SAFE_INTEGER;
+    var oldestPerson;
+    for(const person of people)
+    {
+        if('yearOfDeath' in person)
+        {
+            var tempAge = person.yearOfDeath - person.yearOfBirth;
+            if(tempAge > oldestAge)
+            {
+                oldestAge = tempAge;
+                oldestPerson = person;
+            }
+        }
+        else
+        {
+            const currentDate = new Date();
+            var tempAge = currentDate.getFullYear() - person.yearOfBirth;
+            if(tempAge > oldestAge)
+            {
+                oldestAge = tempAge;
+                oldestPerson = person;
+            }
+        }
+    }
+    return oldestPerson;
 };
 
 // Do not edit below this line

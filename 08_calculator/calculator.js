a207d7e20219850f2549df80b7dc6f1ef956c0f3
GSinghh <Gbassans@yahoo.com>
2023-08-29 09:42:03 -0700

    Completed exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,41 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numArray) {
+	var total = 0;
+  arrayLen = numArray.length;
+  for(var i = 0; i < arrayLen; i++)
+  {
+    total += numArray[i];
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(numArray) {
+  var total = 1;
+  for(var i = 0; i < numArray.length; i++)
+  {
+    total *= numArray[i];
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+  return num ** power;
 };
 
-const factorial = function() {
+const factorial = function(number) {
+  var total = 1;
+  for(var i = 1; i <= number; i++)
+  {
+    total *= i;
+  }
+  return total;
 	
 };
 

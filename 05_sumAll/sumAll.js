a1510634aa8a08a48adab78a3388f5547d7b24a0
GSinghh <Gbassans@yahoo.com>
2023-08-29 08:41:26 -0700

    Completed exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,30 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if(!Number.isInteger(num1) || !Number.isInteger(num2)) 
+    {
+        return "ERROR";
+    }
+    if(num1 <= 0 || num2 <= 0)
+    {
+        return "ERROR";
+    }
+
+    var total = 0;
+    if (num1 > num2)
+    {
+        for(var i = num2; i <= num1; i++)
+        {
+            total += i;
+        }
+        return total;
+    }
+    else
+    {
+        for(var i = num1; i <= num2; i++)
+        {
+            total += i;
+        }
+        return total;
+    }
 
 };
 

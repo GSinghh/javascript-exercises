8b79918503237e5bf8c6ea6f898ecf95703a397f
GSinghh <Gbassans@yahoo.com>
2023-08-29 08:26:33 -0700

    Completed exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(numArray, ...numDelete) {
+    const newArray = [];
+    numArray.forEach(element => {
+        if (!numDelete.includes(element))
+        {
+            newArray.push(element);
+        }
+    });
 
+    return newArray;
 };
 
 // Do not edit below this line
